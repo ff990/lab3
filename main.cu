a7e3535988b64056975f9d83e35c44a66961cbc1
tax1997 <62441869+tax1997@users.noreply.github.com>
2021-11-05 18:58:28 -0700

    Update main.cu

--- main.cu
+++ main.cu
@@ -112,7 +112,7 @@ int main (int argc, char *argv[])
 
     printf("Verifying results..."); fflush(stdout);
 
-    //verify(A_h, B_h, C_h, matArow, matAcol, matBcol);
+    verify(A_h, B_h, C_h, matArow, matAcol, matBcol);
 
 
     // Free memory ------------------------------------------------------------
