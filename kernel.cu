13038985bc8175ef7b43cdb30e2e3369f443a367
tax1997 <62441869+tax1997@users.noreply.github.com>
2021-11-05 17:32:53 -0700

    Update kernel.cu

--- kernel.cu
+++ kernel.cu
@@ -53,27 +53,9 @@ __global__ void mysgemm(int m, int n, int k, const float *A, const float *B, flo
     if (Row<m && Col<n) C[Row*n+Col] = Cvalue;    
 }
 
-void basicSgemm(char transa, char transb, int m, int n, int k, float alpha, const float *A, int lda, const float *B, int ldb, float beta, float *C, int ldc)
+void basicSgemm(int m, int n, int k, const float *A, const float *B, float *C)
 {
-    if ((transa != 'N') && (transa != 'n')) {
-	printf("unsupported value of 'transa'\n");
-    	return;
-    }
-
-    if ((transb != 'N') && (transb != 'n')) {
-	printf("unsupported value of 'transb'\n");
-	return;
-    }
-
-    if ((alpha - 1.0f > 1e-10) || (alpha - 1.0f < -1e-10)) {
-	printf("unsupported value of alpha\n");
-	return;
-    }
-
-    if ((beta - 0.0f > 1e-10) || (beta - 0.0f < -1e-10)) {
-	printf("unsupported value of beta\n");
-	return;
-    }
+    
 
     // Initialize thread block and kernel grid dimensions ---------------------
 
